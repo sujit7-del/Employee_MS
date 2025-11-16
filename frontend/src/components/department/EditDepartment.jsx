56129dc413ff87429fff71e96c4e3abd711012a3
Sujit Parte <partesujit007@gmail.com>
2025-11-16 21:43:05 +0530

    Update EditDepartment.jsx

--- frontend/src/components/department/EditDepartment.jsx
+++ frontend/src/components/department/EditDepartment.jsx
@@ -13,7 +13,7 @@ const EditDepartment = () => {
       setDepLoading(true);
       try {
         const responnse = await axios.get(
-          `http://localhost:5000/api/department/${id}`,
+          `https://employee-ms-server-eomf.onrender.com/api/department/${id}`,
           {
             headers: {
               Authorization: `Bearer ${localStorage.getItem("token")}`,
@@ -44,7 +44,7 @@ const EditDepartment = () => {
     e.preventDefault();
     try {
       const response = await axios.put(
-        `http://localhost:5000/api/department/${id}`,
+        `https://employee-ms-server-eomf.onrender.com/api/department/${id}`,
         department,
         {
           headers: {
