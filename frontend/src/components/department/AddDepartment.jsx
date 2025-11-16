06c921243400834d12152dd7179e034718d038f6
Sujit Parte <partesujit007@gmail.com>
2025-11-16 21:42:02 +0530

    Update AddDepartment.jsx

--- frontend/src/components/department/AddDepartment.jsx
+++ frontend/src/components/department/AddDepartment.jsx
@@ -17,7 +17,7 @@ const AddDepartment = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            const response = await axios.post('http://localhost:5000/api/department/add', department, {
+            const response = await axios.post('https://employee-ms-server-eomf.onrender.com/api/department/add', department, {
                 headers: {
                     "Authorization" : `Bearer ${localStorage.getItem('token')}`
                 }
