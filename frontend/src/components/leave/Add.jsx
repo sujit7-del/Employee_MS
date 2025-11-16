1a52dece9520a798fae3affce61773265b3f757c
Sujit Parte <partesujit007@gmail.com>
2025-11-16 21:35:46 +0530

    Update Add.jsx

--- frontend/src/components/leave/Add.jsx
+++ frontend/src/components/leave/Add.jsx
@@ -22,7 +22,7 @@ const Add = () => {
 
     try {
         const response = await axios.post(
-          `http://localhost:5000/api/leave/add`,leave,
+          `https://employee-ms-server-eomf.onrender.com/api/leave/add`,leave,
           {
             headers: {
               "Authorization": `Bearer ${localStorage.getItem("token")}`,
