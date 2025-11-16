b4d9eb638ab87bfedf3b053faf471bca2cf15f47
Sujit Parte <partesujit007@gmail.com>
2025-11-16 21:39:54 +0530

    Update Edit.jsx

--- frontend/src/components/employee/Edit.jsx
+++ frontend/src/components/employee/Edit.jsx
@@ -27,7 +27,7 @@ const Edit = () => {
     const fetchEmployee = async () => {
       try {
         const responnse = await axios.get(
-          `http://localhost:5000/api/employee/${id}`,
+          `https://employee-ms-server-eomf.onrender.com/api/employee/${id}`,
           {
             headers: {
               Authorization: `Bearer ${localStorage.getItem("token")}`,
@@ -65,7 +65,7 @@ const Edit = () => {
 
     try {
       const response = await axios.put(
-        `http://localhost:5000/api/employee/${id}`,
+        `https://employee-ms-server-eomf.onrender.com/api/employee/${id}`,
         employee,
         {
           headers: {
