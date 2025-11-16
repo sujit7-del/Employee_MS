ef15324fd18bc47e1efc957e44e769853e422d5c
Sujit Parte <partesujit007@gmail.com>
2025-11-16 21:34:46 +0530

    Update View.jsx

--- frontend/src/components/salary/View.jsx
+++ frontend/src/components/salary/View.jsx
@@ -12,7 +12,7 @@ const View = () => {
 
   const fetchSalareis = async () => {
     try {
-      const response = await axios.get(`http://localhost:5000/api/salary/${id}/${user.role}`, {
+      const response = await axios.get(`https://employee-ms-server-eomf.onrender.com/api/salary/${id}/${user.role}`, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
