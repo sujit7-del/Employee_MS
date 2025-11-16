0eb53d3b488512c141af7f7d3c627ec3cb26bdad
Sujit Parte <partesujit007@gmail.com>
2025-11-16 21:46:14 +0530

    Update Setting.jsx

--- frontend/src/components/EmployeeDashboard/Setting.jsx
+++ frontend/src/components/EmployeeDashboard/Setting.jsx
@@ -27,7 +27,7 @@ const Setting = () => {
     } else {
       try {
         const response = await axios.put(
-          "http://localhost:5000/api/setting/change-password",
+          "https://employee-ms-server-eomf.onrender.com/api/setting/change-password",
           setting,
           {
             headers: {
