97044d1ee12ed9f8dfc50c8e076f91ae6abd3c05
Sujit Parte <partesujit007@gmail.com>
2025-11-16 21:33:15 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://employee-ms-server-eomf.onrender.com/api/auth/login",
         { email, password }
       );
       if(response.data.success) {
